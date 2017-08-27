dcb6ef6dbb45cc157e765cd82b5217f44ed0e048
Yuliia <@DESKTOP-6222HOQ>
2017-08-27 18:01:09 +0300

    init commit

---
+++ task_5.9/task_5.9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_5._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 50;
+
+            for (int i = 1; i <= 50; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    continue;
+                }
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
